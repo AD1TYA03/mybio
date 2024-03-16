84e5cdc312ec49899f5df6b26740b6325d6a388f
AD1TYA03 <saaditya17@gmail.com>
2024-03-16 22:16:38 +0530

    about page styling

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,9 +1,16 @@
+import './About.css';
+import Adi2 from '../../assets/Adi2.png';
 
 
 function About() {
-  return (
-    <div>About</div>
-  )
+  return <div className="about-container">
+     <div><h2>Soft Skills</h2></div>
+     <div>
+     <h1>About Me</h1>
+     <img src={Adi2} alt="my image" />
+     </div>
+     <div><h2>Hard Skills</h2></div>
+  </div>;
 }
 
-export default About
+export default About;
